04ec6abb2b9396ff88e801019969482e24611b27
Waincey <waincey.chan@gmail.com>
2018-07-11 16:42:40 +0000

    automatically backed up by learn

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -1,8 +1,34 @@
+def simple_curfew_checker(time)
+  if time == 11
+    puts "You're in trouble."
+    elsif time >11
+    puts "You're late."
+  else time <11
+    puts "You have time to keep having fun"
+  end
+end
+simple_curfew_checker()
+
 def curfew_checker(time)
-  if time 4<5
-    puts "Four is less than five."
-    
-    
+  if time <11
+    puts "Keep having fun!"
+    elsif time == 11
+    puts "You're in trouble."
+  else time >11
+    puts "You're late!"
   end
 end
 curfew_checker()
+
+def complex_curfew_checker(time)
+  if time <== 11
+    puts "Keep having fun!"
+    elsif time == 11
+    puts "You're in trouble."
+  else time >11 
+    puts "You're late!"
+  end
+end
+complex_curfew_checker()
+
+def deluxe_curfew_checker(time)
